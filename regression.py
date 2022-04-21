4c07771ad7017e18c7afa7da075304d9a0189d9f
quratulaen.ikram <quratulaen.ikram@gmail.com>
2022-04-21 11:40:34 +0100

    update regression

--- regression.py
+++ regression.py
@@ -15,7 +15,7 @@ df.drop('product_description', axis=1, inplace=True)
 
 
 X = df[['id', 'category', 'location']]
-y = df['price']
+y = df['price'].fillna(0, inplace=True)
 
 # Creating instance of one-hot-encoder
 encoder = OneHotEncoder(handle_unknown='ignore')
@@ -23,15 +23,21 @@ encoder = OneHotEncoder(handle_unknown='ignore')
 # Perform one-hot encoding on 'category' and 'location' columns 
 encoder_df1 = pd.DataFrame(encoder.fit_transform(X[['category']]).toarray())
 encoder_df2 = pd.DataFrame(encoder.fit_transform(X[['location']]).toarray())
+#encoder_df1.columns = encoder.get_feature_names(['category_encoded'])
+#encoder_df2.columns = encoder.get_feature_names(['location_encoded'])
 
 #merge one-hot encoded columns back with original DataFrame
+final_df = X.join(encoder_df2)
 final_df = X.join(encoder_df1)
+final_df.drop(['category', 'location', 'id'], axis=1, inplace=True)
+final_df.fillna(0, inplace=True)
 
-print(df.head())
+
+X_train, X_test, y_train, y_test = train_test_split(final_df, y, test_size=0.4, random_state=101)
+X_train = X_train.reshape(1, -1)
+X_test = X_test.reshape(1, -1)
 
 model = linear_model.LinearRegression()
 
-#model.fit(X, y)
-#y_pred = model.predict(X)
-
-#print(y_pred[:5], "\n", y[:5])
+model.fit(X_train, y_train)
+model.predict(X_test)
